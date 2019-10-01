e01835d735f7e1719e91f37db13fc085af6258b9
Mihir <mwadekar2000@gmail.com>
2019-09-30 17:15:52 -0700

    Initial commit

---
+++ Plots/Freedom vs GDP/Freedom vs GDP.R
@@ -0,0 +1,36 @@
+library(ggplot2)
+library(dplyr)
+library(plotly)
+
+# Set plotly config
+Sys.setenv("plotly_username"="thatonenerdy")
+Sys.setenv("plotly_api_key"="1b4rzj9N1gNS8DKN5jaX")
+
+# Rad in the CSV files
+hdi <- read.csv('hdi.csv')
+population <- read.csv('population.csv')
+
+# Refining the Data
+population <- subset(population, select = c(Country.Name, X2015))
+names(population) <- c("Country", "X2015")
+data <- merge(hdi, population)
+colnames(data)[colnames(data)=="X2015"] <- "Population"
+
+# Creating the plot
+p <- data %>%
+  mutate(Population=round(Population/1000000,2)) %>%
+  
+  arrange(desc(Population)) %>%
+  mutate(Country = factor(Country, Country)) %>%
+  
+  mutate(text = paste("Country: ", Country)) %>%
+  
+  ggplot( aes(x=Freedom, y=Economy..GDP.per.Capita., size = Population, color = Region, text=text)) +
+  geom_point(alpha=0.7) +
+  scale_size(range = c(1.4, 19), name="Population") +
+  theme(legend.position="top")
+
+# turn ggplot interactive with plotly
+pp <- ggplotly(p, tooltip="text")
+pp
+
---
+++ README.md
@@ -0,0 +1,13 @@
+# Introduction
+This repo is a starter project to dabble in R. It takes in population and HDI data and attempts to find correlations between them.
+
+# Motivation
+R is a pretty cool language and in order to get some hands on experience, I worked on this project. Bubble charts are something I've loved ever since I saw them on TED talks, thus my first project in R, had to involve their usage.
+
+# Libraries
+- ggplot2
+- dplyr
+- plotly
+
+# Author
+Mihir Wadekar
